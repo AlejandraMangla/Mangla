5761efae1f00461bd581fcf4f7b2a4a81842d7f6
Ramos365 <moisesmaro0123@gmail.com>
2025-02-27 07:06:56 -0600

    implementación de los dos graficos de barra

--- assets/js/servicios.js
+++ assets/js/servicios.js
@@ -1,4 +1,5 @@
-const ctx = document.getElementById("myChart1").getContext("2d");
+//--------------Grafico de oxigeno-----------------
+const ctx = document.getElementById("myChart").getContext("2d");
 
 const data = {
   labels: ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Hoy"],
@@ -34,3 +35,41 @@ const config = {
 };
 
 const myChart = new Chart(ctx, config);
+
+//--------------------Grafico co2--------------
+const ctx2 = document.getElementById("myChart2").getContext("2d");
+
+const data2 = {
+  labels: ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Hoy"],
+  datasets: [
+    {
+      label: "Valores",
+      data: [0.0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4], // Aquí se actualizarán los datos
+      backgroundColor: "rgba(0, 255, 255, 0.8)", // Color de las barras
+      borderWidth: 0,
+    },
+  ],
+};
+
+const config2 = {
+  type: "bar",
+  data: data2,
+  options: {
+    indexAxis: "y", // Esto hace que las barras sean horizontales
+    responsive: true,
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: { color: "white" }, // Color de los números del eje X
+      },
+      y: {
+        ticks: { color: "white" }, // Color de las etiquetas del eje Y
+      },
+    },
+    plugins: {
+      legend: { display: false }, // Oculta la leyenda
+    },
+  },
+};
+
+const myChart2 = new Chart(ctx2, config2);
